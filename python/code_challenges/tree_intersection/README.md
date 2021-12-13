e85e619875328299cfbbbad88764990b0b210775
yaseen1998 <yasseenkouthe@eng.hu.edu>
2021-12-14 00:15:46 +0200

    intersection

---
+++ python/code_challenges/tree_intersection/README.md
@@ -0,0 +1,23 @@
+# Challenge Summary
+<!-- Description of the challenge -->
+Write a function called tree_intersection that takes two binary trees as parameters.
+
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+<img src='tree.jpg'>
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+Big O :
+time complexit : O(n1+n2)
+n1 :length tree1
+n2: lenght tree 2
+space complexit : O(n)
+
+## Solution
+<!-- Show how to run your code, and examples of it in action -->
+add tree1 to hash
+add(tree1) -> conver tree1 to hashmap
+contains(ree2) -> check every element is exist on hashmap
+## test
+test 1  there is common between two tree
+test 2 there is not common between two tree
--- python/code_challenges/tree_intersection/tree_intersection/tree_intersection.py
+++ python/code_challenges/tree_intersection/tree_intersection/tree_intersection.py
@@ -1,28 +1,24 @@
-from binary_tree import Binary_tree
-from linkedlist import LinkedList
+# from binary_tree import Binary_tree
 
-tree1 = Binary_tree(20)
-tree1.add(40)
-tree1.add(10)
-tree1.add(15)
-tree1.add(30)
-result1 = tree1.print_BST()
+# tree1 = Binary_tree(20)
+# tree1.add(40)
+# tree1.add(10)
+# tree1.add(15)
+# tree1.add(30)
+# result1 = tree1.print_BST()
 
-tree2 = Binary_tree(20)
-tree2.add(4)
-tree2.add(19)
-tree2.add(45)
-tree2.add(30)
-result2 = tree2.print_BST()
+# tree2 = Binary_tree(21)
+# tree2.add(4)
+# tree2.add(19)
+# tree2.add(45)
+# tree2.add(31)
+# result2 = tree2.print_BST()
 
 class Intersection:
-    def __init__(self,tree1,tree2):
-        self.tree1 = tree1
-        self.tree2 = tree2
+    def __init__(self):
+        self.result = []
         self.size1 = 1024
         self.map1 = [None]*1024
-        # self.map2 = [None]*1024
-        self.add()
     def hash(self,value):
         sum_asci = 0
         value = str(value)
@@ -33,12 +29,23 @@ class Intersection:
         hash_key = temp_value % self.size1
         return hash_key
 
-    def add(self):
-        for i in self.tree1:
+    def add(self,tree1):
+        for i in tree1:
             hash_key = self.hash(i)
             self.map1[hash_key]= i
 
-hash = Intersection(result1,result2)
-for index,item in enumerate (hash.map1):
-    if item:
-        print(index , item)
+    def contains(self, tree2):
+        for i in tree2:
+            hash_key = self.hash(i)
+            if self.map1[hash_key] == i:
+                self.result.append(i)
+        if len(self.result) == 0:
+            return 'no inersection'
+        return self.result
+
+# hash = Intersection()
+# hash.add(result1)
+# print(hash.contains(result2))
+# # for index,item in enumerate (hash.map1):
+# #     if item:
+# #         print(index , item)
